9708cf7c2df4cda8076d4edf8829ee675f231243
S_Kyadav <078bct087@student.ioepc.edu.np>
2023-01-20 02:10:23 +0545

    s

--- src/run.py
+++ src/run.py
@@ -2,20 +2,26 @@ from flask import Flask, request, jsonify
 import pickle
 import cv2
 import numpy as np
+from os import listdir
 
 app = Flask(__name__)
 app.debug = True
-model = pickle.load(open("models/first_cnn.pkl", 'rb'))
+model = pickle.load(open("models/dide1.pkl", 'rb'))
 
-label = ['bacteria', 'brown', 'smut']
+label = list(listdir("data/PlantVillage"))
 
 @app.route("/api/classify/", methods=["POST"])
 def classify():
+    print("test0")
     img = request.files['image']
+    print("TEST1")
     img.save('temp.jpg')
+    print("TEST2")
     image = cv2.imread('temp.jpg')
-    resized = cv2.resize(image, (180, 180))
+    resized = cv2.resize(image, (255, 255))
+    print("TEST3")
     prediction = model.predict(np.array([resized]))
+    print("TEST4")
     return jsonify({'prediction': label[np.argmax(prediction)]})
 
 
