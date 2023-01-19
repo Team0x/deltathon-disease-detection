2260af977c3723268ee5eaded9730ff3657d383c
sudesh <sudesh@cat>
2023-01-20 00:49:11 +0545

    guijdkflahdsusah

--- src/run.py
+++ src/run.py
@@ -1,12 +1,25 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+import pickle
+import cv2
+import numpy as np
 
 app = Flask(__name__)
+app.debug = True
+model = pickle.load(open("models/first_cnn.pkl", 'rb'))
 
-@app.route("/api/classify/", methods=["POST", "GET"])
+label = ['bacteria', 'brown', 'smut']
+
+@app.route("/api/classify/", methods=["POST"])
 def classify():
-    return request.url
+    img = request.files['image']
+    img.save('temp.jpg')
+    image = cv2.imread('temp.jpg')
+    resized = cv2.resize(image, (180, 180))
+    prediction = model.predict(np.array([resized]))
+    return jsonify({'prediction': label[np.argmax(prediction)]})
+
 
 
 @app.route("/")
 def index_test():
-    return "Test"
+    return "test"
